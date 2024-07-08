04caf9072170489d5101c60264a389db7f62411d
lakshan <lakshanrashmika16302@gmail.com>
2024-07-09 00:10:10 +0530

    updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,64 +1,80 @@
-// import { Text, View,StyleSheet,TextInput,Image,Button,Alert } from "react-native";
-import {SafeAreaView, StyleSheet, View, Text, Image} from 'react-native';
+import { Text, View,StyleSheet,SafeAreaView,Image,Button,Alert } from "react-native";
+// import {SafeAreaView, StyleSheet, View, Text, Image} from 'react-native';
 import COLORS from '../app/colors.js';
+import {PrimaryButton} from '../app/Button.js';
+import {FlatList} from 'react-native-gesture-handler';
+
 
 
 import React from "react";
 
 export default function Index() {
   return (
-    <View style={style.cartCard}>
-    <Image  style={{height: 80, width: 80}} />
-    <View
-      style={{
-        height: 100,
-        marginLeft: 10,
-        paddingVertical: 20,
-        flex: 1,
-      }}>
-      <Text style={{fontWeight: 'bold', fontSize: 16}}></Text>
-      <Text style={{fontSize: 13, color: COLORS.grey}}>
-        
-      </Text>
-      {/* <Text style={{fontSize: 17, fontWeight: 'bold'}}>${item.price}</Text> */}
-    </View>
-    <View style={{marginRight: 20, alignItems: 'center'}}>
-      <Text style={{fontWeight: 'bold', fontSize: 18}}>3</Text>
-      <View style={style.actionBtn}>
-        {/* <Icon name="remove" size={25} color={COLORS.white} />
-        <Icon name="add" size={25} color={COLORS.white} /> */}
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View style={{height: 400}}>
+        <Image
+          style={{
+            width: '100%',
+            resizeMode: 'contain',
+            top: -50,
+          }}
+          source={require('../assets/images/Frame40.png')}
+        />
       </View>
-    </View>
-  </View>
+      <View style={style.textContainer}>
+        <View>
+          <Text style={{ marginTop: 150, fontSize: 50, fontWeight: 'bold', textAlign: 'center'}}>
+            Delicious Food
+          </Text>
+          <Text
+            style={{
+              marginBottom: 10,
+              fontSize: 18,
+              textAlign: 'center',
+              color: COLORS.grey,
+            }}>
+            We help you to find best and delicious food
+          </Text>
+        </View>
+        <View style={style.indicatorContainer}>
+          <View style={style.currentIndicator} />
+          <View style={style.indicator} />
+          <View style={style.indicator} />
+        </View>
+        <PrimaryButton
+          title="Get Started"
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const style = StyleSheet.create({
-  header: {
-    paddingVertical: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginHorizontal: 20,
+  textContainer: {
+    flex: 1,
+    paddingHorizontal: 50,
+    justifyContent: 'space-between',
+    paddingBottom: 80,
   },
-  cartCard: {
-    height: 100,
-    elevation: 15,
-    borderRadius: 10,
-    backgroundColor: COLORS.white,
-    marginVertical: 10,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  actionBtn: {
-    width: 80,
-    height: 30,
-    backgroundColor: COLORS.primary,
-    borderRadius: 30,
-    paddingHorizontal: 5,
-    flexDirection: 'row',
+  indicatorContainer: {
+    height: 50,
+    flex: 1,
     justifyContent: 'center',
-    alignContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  currentIndicator: {
+    height: 12,
+    width: 30,
+    borderRadius: 10,
+    backgroundColor: COLORS.primary,
+    marginHorizontal: 5,
+  },
+  indicator: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: COLORS.grey,
+    marginHorizontal: 5,
   },
 });

66a68217310839570407348081f0795f3a6aa44e
lakshan <lakshanrashmika16302@gmail.com>
2024-07-09 13:16:24 +0530

    updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,15 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {StatusBar} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import COLORS from '../app/consts/colors';
-import BottomNavigator from '../app/navigation/BottomNavigator';
-import OnBoardScreen from '../app/screens/onBorardScreen';
+import { Text, View } from "react-native";
 
-const Stack = createStackNavigator();
-
-const index = () => {
+export default function Index() {
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="BoardScreen" component={OnBoardScreen} />
-        <Stack.Screen name="Home" component={BottomNavigator} />
-        {/* <Stack.Screen name="DetailsScreen" component={DetailsScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
   );
-};
-
-export default index;
+}

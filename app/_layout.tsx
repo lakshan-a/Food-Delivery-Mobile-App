9b4b39bc0f93920297d3d44cf4a1b5fe596531e2
lakshan <lakshanrashmika16302@gmail.com>
2024-07-11 11:18:23 +0530

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
+import React from 'react';
+
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
-}
+    return (
+        <Stack>
+            <Stack.Screen name="getStart" options={{ headerShown: false }} />
+            <Stack.Screen name="Login" options={{ headerShown: false }} />
+            <Stack.Screen name="home" options={{ headerShown: false }} />
+            <Stack.Screen name="productDetail" options={{ headerShown: false }} />
+        </Stack>
+    );
+};
